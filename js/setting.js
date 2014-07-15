d1598326a4fbdb806619b4e345db362b94865b33
kiichi kato <kipatto@yahoo.co.jp>
2014-07-15 23:05:40 +0900

    葉山向けアイコン変更

--- js/setting.js
+++ js/setting.js
@@ -18,42 +18,42 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* hayama cofog */
-  /* 議会費 */
-  '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '1-1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  /* 総務費 */
-  '2': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-1': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
-  /* 民生費 */
-  '3': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-1': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
-  /* 衛生費 */
-  '4': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#D33673' },
-  '4-1': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#D33673' },
-  /* 農林水産業費 */
-  '5': { icon: 'icons/environment.svg', color: '#C75746', bcolor: '#2A3A03' },
-  '5-1': { icon: 'icons/tree.svg', color: '#C75746', bcolor: '#2A3A03'  },
-  /* 商工費 */
-  '6': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
-  '6-1': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
-  /* 土木費 */
-  '7': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
-  '7-1': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
-  /* 消防費 */
-  '8': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
-  '8-1': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#C75746' },
-  /* 教育費 */
-  '9': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
-  '9-1': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
-  /* 災害復旧費 */
-  '10': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
-  '10-1': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
-  /* 公債費 */
-  '11': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
-  '11-1': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
-  /* 予備費 */
-  '13': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
-  '13-1': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  /* 議会費 vote.svg */
+  '1': { icon: 'icons/hayama/vote.svg', color: '#C75746', bcolor: '#935B3B' },
+  '1-1': { icon: 'icons/hayama/vote.svg', color: '#C75746', bcolor: '#935B3B' },
+  /* 総務費 publicaffairs.svg */
+  '2': { icon: 'icons/hayama/publicaffairs.svg', color: '#C75746', bcolor: '#0AB971' },
+  '2-1': { icon: 'icons/hayama/publicaffairs.svg', color: '#C75746', bcolor: '#0AB971' },
+  /* 民生費 family.svg */
+  '3': { icon: 'icons/hayama/family.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '3-1': { icon: 'icons/hayama/family.svg', color: '#C75746', bcolor: '#4E6D00' },
+  /* 衛生費 health.svg */
+  '4': { icon: 'icons/hayama/health.svg', color: '#C75746', bcolor: '#D33673' },
+  '4-1': { icon: 'icons/hayama/health.svg', color: '#C75746', bcolor: '#D33673' },
+  /* 農林水産業費 nougyou.svg */
+  '5': { icon: 'icons/hayama/nougyou.svg', color: '#C75746', bcolor: '#2A3A03' },
+  '5-1': { icon: 'icons/hayama/nougyou.svg', color: '#C75746', bcolor: '#2A3A03'  },
+  /* 商工費 shoukou.svg */
+  '6': { icon: 'icons/hayama/shoukou.svg', color: '#C75746', bcolor: '#EC2406' },
+  '6-1': { icon: 'icons/hayama/shoukou.svg', color: '#C75746', bcolor: '#EC2406' },
+  /* 土木費 doboku.dvg */
+  '7': { icon: 'icons/hayama/doboku.svg', color: '#C75746', bcolor: '#938626' },
+  '7-1': { icon: 'icons/hayama/doboku.svg', color: '#C75746', bcolor: '#938626' },
+  /* 消防費 syoubou_bousai.svg */
+  '8': { icon: 'icons/hayama/syoubou_bousai.svg', color: '#C75746', bcolor: '#C75746' },
+  '8-1': { icon: 'icons/hayama/syoubou_bousai.svg', color: '#C75746', bcolor: '#C75746' },
+  /* 教育費 education.svg */
+  '9': { icon: 'icons/hayama/education.svg', color: '#C75746', bcolor: '#D33673' },
+  '9-1': { icon: 'icons/hayama/education.svg', color: '#C75746', bcolor: '#D33673' },
+  /* 災害復旧費 saigai_fukkyu.svg */
+  '10': { icon: 'icons/hayama/saigai_fukkyu.svg', color: '#C75746', bcolor: '#790586' },
+  '10-1': { icon: 'icons/hayama/saigai_fukkyu.svg', color: '#C75746', bcolor: '#790586' },
+  /* 公債費 japanese-money.svg */
+  '11': { icon: 'icons/hayama/japanese-money.svg', color: '#C75746', bcolor: '#790586' },
+  '11-1': { icon: 'icons/hayama/japanese-money.svg', color: '#C75746', bcolor: '#790586' },
+  /* 予備費 money.svg */
+  '13': { icon: 'icons/hayama/money.svg', color: '#C75746', bcolor: '#790586' },
+  '13-1': { icon: 'icons/hayama/money.svg', color: '#C75746', bcolor: '#790586' }
 
 };
 
